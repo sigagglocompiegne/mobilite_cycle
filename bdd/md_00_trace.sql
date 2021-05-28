b45aadc369d67aff47bbdb7fca77bc31855b68c1
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-05-28 13:53:26 +0200

    Update md_00_trace.sql

--- bdd/md_00_trace.sql
+++ bdd/md_00_trace.sql
@@ -1,2 +1,10 @@
+/*Mobilité douce V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* md_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Bodet Grégory */
+
 -- #################################################################### SUIVI CODE SQL ####################################################################
 
