0745603110afe8f4fc142a1371b509fc440f303f
JulienH77 <84439263+JulienH77@users.noreply.github.com>
2021-07-07 10:28:38 +0200

    Update md_10_squelette.sql

--- bdd/md_10_squelette.sql
+++ bdd/md_10_squelette.sql
@@ -145,8 +145,8 @@ CREATE SEQUENCE m_mobilite_3v.mob_lk_gid
 --############################################################ OBJETS ##################################################
 CREATE SEQUENCE m_mobilite_3v.mob_media_seq_gid
     INCREMENT 1
-    START 1
-    MINVALUE 1
+    START 200
+    MINVALUE 200
     MAXVALUE 9223372036854775807
     CACHE 1;
 
