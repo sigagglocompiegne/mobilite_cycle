c491af786652af21604d21ecee25b12ba28e42fa
JulienH77 <84439263+JulienH77@users.noreply.github.com>
2021-07-26 16:33:49 +0200

    Update md_21_vues_xapps.sql

--- bdd/md_21_vues_xapps.sql
+++ bdd/md_21_vues_xapps.sql
@@ -51,26 +51,30 @@ DROP TABLE if exists x_apps.xapps_an_v_mob_erreur;
 
 --##############################################################OUVELEC#############################################################
 -- Vue d'affichage pour un affichage distinct entre les différents mode d aménagements des tronçons
-
 CREATE OR REPLACE VIEW x_apps.xapps_geo_v_mob_troncon_affiche
  AS
  WITH req_t AS (
+	   -- selection des tronçons de droite où les tronçons de gauche sont non aménagés et non concernés
 	   SELECT tr.idtroncon, tr.ame_d AS ame, tr.avanc_d AS avanc, tr.geom 
 	   FROM m_mobilite_3v.geo_mob_troncon tr 
 	   WHERE tr.ame_g::text = 'ZZ'::text OR tr.ame_g::text = '10'::text OR tr.ame_g::text = '11'::text
         UNION ALL
+	   -- selection des tronçons de droite où les tronçons de gauche sont égaux à ceux de droite et ceux de gauche sont aménagés. Pour pouvoir les décaler
            SELECT tr.idtroncon, tr.ame_d AS ame, tr.avanc_d AS avanc, st_offsetcurve(tr.geom, - 4::double precision, 'quad_segs=4 join=round'::text) AS geom 
 	   FROM m_mobilite_3v.geo_mob_troncon tr 
 	   WHERE tr.ame_g::text = tr.ame_d::text AND tr.ame_g::text <> 'ZZ'::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text
         UNION ALL
+	   -- selection des tronçons de gauche où les troncçons de droite sont égaux à ceux de gauche et ceux de gauche sont aménagés. Pour pouvoir les décaler
            SELECT tr.idtroncon, tr.ame_g AS ame, tr.avanc_g AS avanc, st_offsetcurve(tr.geom, 4::double precision, 'quad_segs=4 join=round'::text) AS geom 
 	   FROM m_mobilite_3v.geo_mob_troncon tr 
 	   WHERE tr.ame_g::text = tr.ame_d::text AND tr.ame_g::text <> 'ZZ'::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text
         UNION ALL
+	   -- selection des tronçons de droite qui sont différents de ceux de gauche et ceux de gauche sont aménagés. Pour pouvoir les décaler
            SELECT tr.idtroncon, tr.ame_d AS ame, tr.avanc_d AS avanc, st_offsetcurve(tr.geom, - 4::double precision, 'quad_segs=4 join=round'::text) AS geom 
 	   FROM m_mobilite_3v.geo_mob_troncon tr 
 	   WHERE tr.ame_g::text <> tr.ame_d::text AND tr.ame_g::text <> 'ZZ'::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text
         UNION ALL
+	   -- selection des tronçons de gauche qui sont différents de ceux de droite et ceux de gauche sont aménagés. Pour pouvoir les décaler
            SELECT tr.idtroncon, tr.ame_g AS ame, tr.avanc_g AS avanc, st_offsetcurve(tr.geom, 4::double precision, 'quad_segs=4 join=round'::text) AS geom 
 	   FROM m_mobilite_3v.geo_mob_troncon tr 
 	   WHERE tr.ame_g::text <> tr.ame_d::text AND tr.ame_g::text <> 'ZZ'::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text
@@ -84,6 +88,7 @@ ALTER TABLE x_apps.xapps_geo_v_mob_troncon_affiche OWNER TO sig_stage;
 -- Vue de modélisation des noeuds des tronçons purement cartographique pour géo
 CREATE OR REPLACE VIEW m_mobilite_3v.geo_v_mob_noeud
  AS
+  -- selection des points de départ et d'arrivée de chaque tronçon
   SELECT tr.idtroncon, st_union(st_startpoint(tr.geom), st_endpoint(tr.geom)) AS geom 
   FROM m_mobilite_3v.geo_mob_troncon tr;
 ALTER TABLE m_mobilite_3v.geo_v_mob_noeud OWNER TO sig_stage;
@@ -94,14 +99,17 @@ ALTER TABLE m_mobilite_3v.geo_v_mob_noeud OWNER TO sig_stage;
 CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab1_apc
  AS
  WITH req_droite AS (
+	     -- selection de la somme (en km) des longueurs des tronçons de droite aménagés et en service sur l'ensemble de l'APC
              SELECT 1 AS id, round(sum(geo_mob_troncon.long_m::numeric) / 1000::numeric, 2) AS long_km 
 	     FROM m_mobilite_3v.geo_mob_troncon 
 	     WHERE geo_mob_troncon.ame_d::text <> '10'::text AND geo_mob_troncon.ame_d::text <> '11'::text AND geo_mob_troncon.ame_d::text <> 'ZZ'::text AND geo_mob_troncon.avanc_d::text = '50'::text), 
       req_gauche AS (
+	     -- selection de la somme (en km) des longueurs des tronçons de gauche aménagés et en service sur l'ensemble de l'APC
              SELECT 1 AS id, round(sum(geo_mob_troncon.long_m::numeric) / 1000::numeric, 2) AS long_km 
 	     FROM m_mobilite_3v.geo_mob_troncon 
 	     WHERE geo_mob_troncon.ame_g::text <> '10'::text AND geo_mob_troncon.ame_g::text <> '11'::text AND geo_mob_troncon.ame_g::text <> 'ZZ'::text AND geo_mob_troncon.avanc_g::text = '50'::text), 
       req_statio AS (
+	     -- selection de la somme des places de stationnement cyclable en service sur l'ensemble de l'APC
              SELECT 1 AS id, sum(geo_mob_lieustatio.capacite) AS nb_statio 
 	     FROM m_mobilite_3v.geo_mob_lieustatio 
 	     WHERE geo_mob_lieustatio.avanc::text = '50'::text)
@@ -116,16 +124,19 @@ ALTER TABLE m_mobilite_3v.xapps_an_v_mob3v_tab1_apc OWNER TO sig_stage;
 CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab1_epci
  AS
  WITH req_droite AS (
+	   -- selection de la somme (en km) des longueurs des tronçons de droite aménagés et en service par EPCI
            SELECT e_1.lib_epci_m AS epci, round(sum(tr.long_m::numeric) / 1000::numeric, 2) AS long_km
            FROM m_mobilite_3v.geo_mob_troncon tr, r_administratif.an_geo g_1, r_osm.geo_osm_epci e_1
            WHERE tr.code_com_d::text = g_1.insee::text AND g_1.epci::text = e_1.cepci::text AND tr.ame_d::text <> '10'::text AND tr.ame_d::text <> '11'::text AND tr.ame_d::text <> 'ZZ'::text AND tr.avanc_d::text = '50'::text
            GROUP BY e_1.lib_epci_m), 
       req_gauche AS (
+	   -- selection de la somme (en km) des longueurs des tronçons de gauche aménagés et en service par EPCI
            SELECT e_1.lib_epci_m AS epci, round(sum(tr.long_m::numeric) / 1000::numeric, 2) AS long_km
            FROM m_mobilite_3v.geo_mob_troncon tr, r_administratif.an_geo g_1, r_osm.geo_osm_epci e_1
            WHERE tr.code_com_g::text = g_1.insee::text AND g_1.epci::text = e_1.cepci::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text AND tr.ame_g::text <> 'ZZ'::text AND tr.avanc_g::text = '50'::text
            GROUP BY e_1.lib_epci_m),
       req_statio AS (
+	   -- selection de la somme des places de stationnement cyclable en service par EPCI
            SELECT e_1.lib_epci_m AS epci, sum(s_1.capacite) AS nb_statio
            FROM m_mobilite_3v.geo_mob_lieustatio s_1, r_administratif.an_geo g_1, r_osm.geo_osm_epci e_1
            WHERE s_1.insee::text = g_1.insee::text AND g_1.epci::text = e_1.cepci::text AND s_1.avanc::text = '50'::text
@@ -149,10 +160,11 @@ ALTER TABLE m_mobilite_3v.xapps_an_v_mob3v_tab1_epci OWNER TO sig_stage;
 
 
 --##############################################################OUVELEC#############################################################
--- Vue permettant d afficher le pourcentage d aménagements cyclables différents dans GEO à l''échelle de l''APC
+-- Vue permettant d'afficher le pourcentage d'aménagements cyclables différents dans GEO à l''échelle de l''APC
 CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_apc
  AS
  WITH req_droite AS (
+	  -- selection du nom de l'aménagement et de la somme (en km) des longueurs des tronçons de droite aménagés et en service sur l'ensemble de l'APC
           SELECT DISTINCT lt_1.code, lt_1.valeur, round(sum(tr.long_m::numeric) / 1000::numeric, 2) AS long_km
           FROM m_mobilite_3v.lt_mob_ame lt_1
               LEFT JOIN m_mobilite_3v.geo_mob_troncon tr ON tr.ame_d::text = lt_1.code::text
@@ -160,10 +172,12 @@ CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_apc
           GROUP BY lt_1.code, lt_1.valeur
           ORDER BY lt_1.code), 
       req_gauche AS (
+	  -- selection du nom de l'aménagement et de la somme (en km) des longueurs des tronçons de gauche aménagés et en service sur l'ensemble de l'APC
           SELECT lt_1.code, lt_1.valeur, round(sum(tr.long_m::numeric) / 1000::numeric, 2) AS long_km
           FROM m_mobilite_3v.geo_mob_troncon tr, m_mobilite_3v.lt_mob_ame lt_1
           WHERE tr.ame_g::text <> 'ZZ'::text AND tr.ame_g::text <> '10'::text AND tr.ame_g::text <> '11'::text AND tr.avanc_g::text = '50'::text AND tr.ame_g::text = lt_1.code::text
           GROUP BY lt_1.code, lt_1.valeur)
+ -- selection de la somme des longueurs (zéro si null) et calcul du pourcentage par aménagement
  SELECT lt.code,
  	lt.valeur AS nom_ame,
         CASE WHEN d.long_km IS NULL THEN 0::numeric
@@ -177,17 +191,19 @@ CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_apc
                END +
                CASE WHEN g.long_km IS NULL THEN 0::numeric
            	    ELSE g.long_km
-               END) / (( WITH req_droite AS (
+               END) / (( WITH req_droite AS ( --------------- calcul du dénominateur
+		       		    -- selection de la somme des tronçons de droite (en km) qui sont aménagés
 				    SELECT 1 AS id, round(sum(geo_mob_troncon.long_m::numeric) / 1000::numeric, 2) AS long_km
 				    FROM m_mobilite_3v.geo_mob_troncon
 				    WHERE geo_mob_troncon.ame_d::text <> '10'::text AND geo_mob_troncon.ame_d::text <> '11'::text AND geo_mob_troncon.ame_d::text <> 'ZZ'::text AND geo_mob_troncon.avanc_d::text = '50'::text),
 			      req_gauche AS (
+				    -- selection de la somme des tronçons de gauche (en km) qui sont aménagés
 				    SELECT 1 AS id, round(sum(geo_mob_troncon.long_m::numeric) / 1000::numeric, 2) AS long_km
 				    FROM m_mobilite_3v.geo_mob_troncon
 				    WHERE geo_mob_troncon.ame_g::text <> '10'::text AND geo_mob_troncon.ame_g::text <> '11'::text AND geo_mob_troncon.ame_g::text <> 'ZZ'::text AND geo_mob_troncon.avanc_g::text = '50'::text)
 			 SELECT d_1.long_km + g_1.long_km AS long_km
 			 FROM req_droite d_1, req_gauche g_1
-			 WHERE d_1.id = g_1.id)) * 100::numeric, 2) AS long_km_p
+			 WHERE d_1.id = g_1.id)) * 100::numeric, 2) AS long_km_p ----- le tout multiplié par 100 pour créer un pourcentage
  FROM m_mobilite_3v.lt_mob_ame lt
      LEFT JOIN req_droite d ON d.code::text = lt.code::text
      LEFT JOIN req_gauche g ON g.code::text = lt.code::text
@@ -200,6 +216,7 @@ ALTER TABLE m_mobilite_3v.xapps_an_v_mob3v_tab2_apc OWNER TO sig_stage;
 CREATE OR REPLACE VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_epci
  AS
  WITH req_tot AS (
+	   -- 
            (SELECT DISTINCT e.lib_epci_m AS epci, lt_1.code, lt_1.valeur, round(sum(tr.long_m::numeric) / 1000::numeric, 2) AS long_km
            FROM m_mobilite_3v.lt_mob_ame lt_1
                LEFT JOIN m_mobilite_3v.geo_mob_troncon tr ON tr.ame_d::text = lt_1.code::text
@@ -527,9 +544,9 @@ COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_apc IS 'Vue permettant d aff
 COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab2_epci IS 'Vue permettant d afficher le pourcentage d''aménagements cyclables par EPCI dans GEO (graphique)';
 COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab3 IS 'Vue tableau de bord pour synthèse nombre total d''itinéraire et leur km (Chiffres clés des itinéraires cyclables) afficher avec le filtre du paramètre global';
 COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab31 IS 'Vue tableau de bord pour synthèse nombre total d''itinéraire et leur km (Chiffres clés des itinéraires cyclables)';
-COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab32 IS 'Vue permettant d afficher un graphique avec le pourcentage des différents aménagements cyclables pour l''itinéraire sélectionné (paramètre global) dans GEO. Attention résultat non vérifié (à faire)';
-COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab11_apc IS 'Vue permettant d afficher le tableau des informations des stationnements cyclables en service dans GEO';
-COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab11_epci IS 'Vue permettant d afficher le tableau des informations des stationnements cyclables en service par EPCI dans GEO';
+COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab32 IS 'Vue permettant d''afficher un graphique avec le pourcentage des différents aménagements cyclables pour l''itinéraire sélectionné (paramètre global) dans GEO. Attention résultat non vérifié (à faire)';
+COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab11_apc IS 'Vue permettant d''afficher le tableau des informations des stationnements cyclables en service dans GEO';
+COMMENT ON VIEW m_mobilite_3v.xapps_an_v_mob3v_tab11_epci IS 'Vue permettant d''afficher le tableau des informations des stationnements cyclables en service par EPCI dans GEO';
 
 COMMENT ON MATERIALIZED VIEW m_mobilite_3v.geo_vmr_mob_iti IS 'Vue matérialisée regénérant les itinéraires à partir des tronçons';
 COMMENT ON MATERIALIZED VIEW m_mobilite_3v.old_geo_vmr_mob_iti IS 'Vue matérialisée regénérant les itinéraires à partir des tronçons';
