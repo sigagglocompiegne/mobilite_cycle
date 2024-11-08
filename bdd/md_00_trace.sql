282c49855f42d51624a5858dc2d863a724ae8409
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-11-08 09:39:26 +0100

    Update md_00_trace.sql

--- bdd/md_00_trace.sql
+++ bdd/md_00_trace.sql
@@ -6,6 +6,7 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Bodet Grégory */
 
+-- 2024-11-08 : GB / Finalisation de la structure et du code d'implémentation
 -- 2024-09-05 : GB / Ajustement structurel suite période test applicative
 -- 2024-02-02 : GB / initialisation du nouveau modèle de gestion des mobilités douces (stationnement cyclables, aménagement, itinéraire, ...)
 
