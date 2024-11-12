6cf62b13bf41d0441eaf409fe4b1a7c9614caf68
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-11-12 11:28:30 +0100

    Update md_21_vues_xapps.sql

--- bdd/md_21_vues_xapps.sql
+++ bdd/md_21_vues_xapps.sql
@@ -19,7 +19,7 @@ drop view if exists m_mobilite_douce.xapps_an_v_iti_ame_pan_tab;
 drop view if exists m_mobilite_douce.xapps_an_v_iti_ame_tab;
 drop view if exists m_mobilite_douce.xapps_an_v_iti_cycl_tab;
 drop view if exists m_mobilite_douce.xapps_an_v_iti_tab;
-drop view if exists xapps_an_v_statio_cycl_tab1;
+drop view if exists m_mobilite_douce.xapps_an_v_statio_cycl_tab1;
 drop view if exists m_mobilite_douce.xapps_geo_v_noeud_troncon_tab;
 
 
