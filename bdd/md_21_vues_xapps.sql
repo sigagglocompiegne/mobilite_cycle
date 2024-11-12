3f459862a79e54d4a2933d68432825b1f3bdc386
gregbodet <37414914+gregbodet@users.noreply.github.com>
2024-11-12 11:24:11 +0100

    Update md_21_vues_xapps.sql

--- bdd/md_21_vues_xapps.sql
+++ bdd/md_21_vues_xapps.sql
@@ -23,3 +23,21 @@ drop view if exists xapps_an_v_statio_cycl_tab1;
 drop view if exists m_mobilite_douce.xapps_geo_v_noeud_troncon_tab;
 
 
+drop materialized view IF EXISTS m_mobilite_douce.xapps_geo_vmr_amgt_cycl_hors_planvelo_arc;
+drop materialized view IF EXISTS m_mobilite_douce.xapps_geo_vmr_iti_cycl;
+drop materialized view IF EXISTS m_mobilite_douce.xapps_geo_vmr_iti_cycl_rep_planvelo_60159_gdpublic;
+drop materialized view IF EXISTS m_mobilite_douce.xapps_geo_vmr_iti_cycl_rep_planvelo_arc_gdpublic;
+drop materialized view IF EXISTS m_mobilite_douce.xapps_geo_vmr_iti_rand;
+
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_pan_typcode;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_acces;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_ame;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_local;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_regime;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_res;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_revet;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_tronc_sens;
+drop MATERIALIZED view if exists m_mobilite_douce.lt_vmr_mob_troncon;
+drop view if exists m_mobilite_douce.xapps_geo_vmr_mob_amgt_troncon;
+drop view if exists m_mobilite_douce.xapps_geo_vmr_mob_amgt_troncon_etat;
+drop view if exists m_mobilite_douce.xapps_geo_vmr_mob_amgt_troncon_requa;
