364f5e8178eae9cdf0b82a3ded24b6821c08abe3
gregbodet <37414914+gregbodet@users.noreply.github.com>
2025-01-14 09:48:43 +0100

    Update md_00_trace.sql

--- bdd/md_00_trace.sql
+++ bdd/md_00_trace.sql
@@ -6,6 +6,7 @@
 /* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Bodet Grégory */
 
+-- 2025-01-14 : GB / Intégration des classes d'objets liés aux équipements vélos (aire de service + équipement hors stationnement)
 -- 2024-12-06 : GB / Ajustement du code pour optimisation des affichages
 -- 2024-11-08 : GB / Finalisation de la structure et du code d'implémentation
 -- 2024-09-05 : GB / Ajustement structurel suite période test applicative
